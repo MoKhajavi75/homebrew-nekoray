c689fc8ea4074fbac20d1ad4fe74137df3a67eb0
tdjnodj <108401163+tdjnodj@users.noreply.github.com>
2023-01-31 17:48:25 +0800

    Update nekoray.rb

--- Formula/nekoray.rb
+++ Formula/nekoray.rb
@@ -1,8 +1,8 @@
 cask "nekoray" do
-  version "2.11"
-  sha256 "36a840c7853a92de91796626c1e08ea6bff9fdb0e0ee9e152903be9cfb6e5af0"
+  version "2.12"
+  sha256 "bf3fc8866206aa1d8fa5cbc4b31cad2aafc0c320ff54cbf740ac6ea4549adbb1"
 
-  url "https://github.com/MatsuriDayo/nekoray/releases/download/#{version}/nekoray-#{version}-2023-01-13-macos-amd64.dmg",
+  url "https://github.com/MatsuriDayo/nekoray/releases/download/#{version}/nekoray-#{version}-2023-01-29-macos-amd64.dmg",
       verified: "github.com/MatsuriDayo/nekoray/"
   name "nekoray"
   desc "Qt based cross-platform GUI proxy configuration manager (backend: v2ray / sing-box)"
