82afa1ac0a0b62a2be10d4c533693407fc7eafd8
tdjnodj <108401163+tdjnodj@users.noreply.github.com>
2023-02-11 18:46:25 +0800

    2.14

--- Casks/nekoray.rb
+++ Casks/nekoray.rb
@@ -1,8 +1,8 @@
 cask "nekoray" do
-    version "2.13"
-    sha256 "cde2ed0f0ffdbd7a97f7e34a2e010fa38f2587f951aa301d6e6d777875f6cf47"
+    version "2.14"
+    sha256 "40a9b22c9f225b2ea663a07d0af31002cb87599edab4e631a10e0e77330ee9d8"
   
-    url "https://github.com/tdjnodj/nekoray/releases/download/#{version}/nekoray-#{version}-2023-01-31-macos-amd64.dmg",
+    url "https://github.com/tdjnodj/nekoray/releases/download/#{version}/nekoray-#{version}-2023-02-04-macos-amd64.dmg",
         verified: "github.com/MatsuriDayo/nekoray/"
     name "nekoray"
     desc "Qt based cross-platform GUI proxy configuration manager (backend: v2ray / sing-box)"
